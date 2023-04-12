feccd87f3df5524293f9046364cccd357d72d86b
Lucia Garavito <gara0066@algonquinlive.com>
2023-04-11 20:55:49 -0400

    adds variables for list and button

---
+++ main.js
@@ -0,0 +1,8 @@
+///**DEFINE VARIABLE TO DISPLAY LIST AND FUNCTIONALITY OF BUTTON */
+const pokeList = document.getElementById('poke-list');
+const loadMoreButton = document.getElementById('more');
+
+// Set initial values to display 20 pokemons**///////////
+let offset = 0;
+let limit = 20;
+
