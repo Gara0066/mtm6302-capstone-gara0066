660e8be63fcefef1cc0c981cdedaf88ae2b636d2
Lucia Garavito <gara0066@algonquinlive.com>
2023-04-11 21:42:20 -0400

    adds a for loop to display  several pokemons

--- main.js
+++ main.js
@@ -6,10 +6,24 @@ const loadMoreButton = document.getElementById('more');
 let offset = 0;
 let limit = 20;
 
-/////*** Uses an async function to request the PokeAPI and display the array of Pokémons***/////////
+/////*** Uses an async function to request the PokeAPI and display Pokémons***/////////
 async function getPokemonCards() {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
     const data = await response.json();
-    return data.results;
+    const pokemonData = data.results;
+    const pokemonCards = pokemonData.map(pokemon => ({
+      name: pokemon.name,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`,
+      types: [],
+    }));
+
+/////////*********Create a for Loop through each Pokémon card to get the name and type***********////////////////
+  for (let i = 0; i < pokemonCards.length; i++) {
+    const pokemon = pokemonCards[i];
+    const typesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+    const typesData = await typesResponse.json();
+    pokemon.types = typesData.types.map(type => type.type.name);
   }
-  
+  return pokemonCards;
+}
+
