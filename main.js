d72d35e06456c2ececf2ee5eb01e5a3e186bcc4f
Lucia Garavito <gara0066@algonquinlive.com>
2023-04-11 21:02:53 -0400

    adds an async function using fetch

--- main.js
+++ main.js
@@ -2,7 +2,14 @@
 const pokeList = document.getElementById('poke-list');
 const loadMoreButton = document.getElementById('more');
 
-// Set initial values to display 20 pokemons**///////////
+//////*** Set initial values to display 20 pokemons******///////////
 let offset = 0;
 let limit = 20;
 
+/////*** Uses an async function to request the PokeAPI and display the array of Pokémons***/////////
+async function getPokemonCards() {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+    const data = await response.json();
+    return data.results;
+  }
+  
